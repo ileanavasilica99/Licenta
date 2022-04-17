2f006ed28a50137ee081b2ba3deae55bb6f08cd2
Ileana Vasilica <vasilicaileana500@gmail.com>
2022-04-17 12:05:37 +0300

    first commit

---
+++ OrientationAndOrganizationApp/src/main/java/ace/ucv/ro/orientationandorganizationapp/OrientationAndOrganizationAppApplication.java
@@ -0,0 +1,13 @@
+package ace.ucv.ro.orientationandorganizationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientationAndOrganizationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrientationAndOrganizationAppApplication.class, args);
+    }
+
+}
---
+++ OrientationAndOrganizationApp/src/test/java/ace/ucv/ro/orientationandorganizationapp/OrientationAndOrganizationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ace.ucv.ro.orientationandorganizationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientationAndOrganizationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
